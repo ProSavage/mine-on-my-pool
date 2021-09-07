9e2b3f126b1d46ed4a2daef5fe6699d06a5d0bda
ProSavage <naman0311@gmail.com>
2021-09-06 22:17:54 -0500

    Initial commit

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	godotenv.Load()
+	token := os.Getenv("TOKEN")
+	discord, err := discordgo.New("Bot " + token)
+	if err != nil {
+		fmt.Println("error creating discord session", err)
+		return
+	}
+
+	discord.AddHandler(messageCreate)
+	discord.Identify.Intents = discordgo.IntentsGuildMessages
+
+	err = discord.Open()
+	if err != nil {
+		fmt.Println("error opening socket", err)
+	}
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	// Cleanly close down the Discord session.
+	discord.Close()
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	fmt.Println("Got a message: " + m.Content)
+
+	// Ignore all messages created by the bot itself
+	// This isn't required in this specific example but it's a good practice.
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+
+	if m.Author.ID == "350817394014552074" {
+		s.ChannelMessageSend(m.ChannelID, "Are you even mining here?")
+	}
+}
